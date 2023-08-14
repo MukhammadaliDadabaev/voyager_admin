2df298909fe912ae327fa55cc0d918c7a2f939ff
Mukhammadali <ali_6017@mail.ru>
2023-08-14 20:35:03 +0500

    created projects model, controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [ProjectController::class, 'index']);
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
